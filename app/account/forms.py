c386e24c88df808122f3793d33077034544e0166
barasamichael <ourdigitaltimes@gmail.com>
2023-11-23 02:30:10 +0300

    Remove Everything

--- app/account/forms.py
+++
@@ -1,4 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, SelectField, TextAreaField, MultipleFileField, 
-        IntegerField)
-from wtforms.validators import DataRequired, Length
